06ecb7b589ad084f6611eec3f9723963f3de7d84
Ruan <ruan6047@gmail.com>
2022-06-26 23:53:55 +0800

    chore: 新增練習用原檔

---
+++ Assets/Scripts/ruan/test/GameManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+namespace ruan.test
+{
+    public class GameManager : MonoBehaviour
+    {
+        public CarController car;
+
+        public List<Power> PowerList = new List<Power>();
+        // Start is called before the first frame update
+        void Start() { }
+
+        // Update is called once per frame
+        void Update() { }
+
+    }
+}
